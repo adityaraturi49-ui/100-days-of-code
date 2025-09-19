3cc85f9ef4a236b315c35ea6c5479fce9a1154ad
adityaraturi49-ui <adityaraturi49@gmail.com>
2025-09-19 19:31:54 +0530

    Add files via upload

---
+++ nsum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int n,i,sum=0;
+printf("enter value of n: ");
+scanf("%d",&n);
+for(i=1;i<=n;i++){
+sum=sum+(2*i-1);
+}
+printf("sum of first %d odd numbers=%d\n",n,sum);
+return 0;
+}
