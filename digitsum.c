b657a6faf1f8d52f7973bf097ffd573d3148c233
adityaraturi49-ui <adityaraturi49@gmail.com>
2025-09-20 19:12:39 +0530

    Add files via upload

---
+++ digitsum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum=0, digit;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    while(n!=0){
+        digit=n%10;
+        sum+=digit;
+        n/=10;
+    }
+    printf("sum of digits is: %d\n",sum);
+    return 0;
+}
