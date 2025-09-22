8d54e5d0c305d5e8ddaa97194950a8fafa472825
adityaraturi49-ui <adityaraturi49@gmail.com>
2025-09-22 19:55:09 +0530

    Add files via upload

---
+++ nseriessum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int n,i;
+double sum=0.0;
+printf("enter number of terms:");
+scanf("%d",&n);
+for(i=1;i<=n;i++){
+sum+=(double)(2*i-1)/(2*i);
+}
+printf("sum of series up to %d terms=%.4lf\n",n,sum);
+return 0;
+}
