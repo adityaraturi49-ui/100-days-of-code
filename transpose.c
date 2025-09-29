4bb7ea4336463e91e8e2506f458e55cd655c0934
adityaraturi49-ui <adityaraturi49@gmail.com>
2025-09-29 20:44:35 +0530

    Add files via upload

---
+++ transpose.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+int m,n;
+printf("enter number of rows and columns:");
+scanf("%d %d",&m,&n);
+int matrix[m][n],transpose[n][m];
+printf("enter elements of the matrix:\n");
+for(int i=0;i<m;i++){
+for(int j=0;j<n;j++){
+scanf("%d",&matrix[i][j]);
+}
+}
+for(int i=0;i<m;i++){
+for(int j=0;j<n;j++){
+transpose[j][i]=matrix[i][j];
+}
+}
+printf("transpose of the matrix:\n");
+for(int i=0;i<n;i++){
+for(int j=0;j<m;j++){
+printf("%d ",transpose[i][j]);
+}
+printf("\n");
+}
+return 0;
+}
