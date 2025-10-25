4c71d365eb0c002cfc9f9ab4397b7e33354c9e77
adityaraturi49-ui <adityaraturi49@gmail.com>
2025-10-25 19:50:55 +0530

    Add files via upload

---
+++ novowel.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+char str[100], result[100];
+int i, j=0;
+printf("enter a string: ");
+fgets(str,100,stdin);
+for(i=0;str[i]!='\0';i++){
+if(str[i]!='a'&&str[i]!='e'&&str[i]!='i'&&str[i]!='o'&&str[i]!='u'&&
+   str[i]!='A'&&str[i]!='E'&&str[i]!='I'&&str[i]!='O'&&str[i]!='U'){
+result[j]=str[i];
+j++;
+}
+}
+result[j]='\0';
+printf("string without vowels: %s",result);
+return 0;
+}
