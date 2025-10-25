90f3e265178481905b735b05656d1daf21fabce4
adityaraturi49-ui <adityaraturi49@gmail.com>
2025-10-25 19:39:41 +0530

    Add files via upload

---
+++ toogle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<ctype.h>
+int main(){
+char str[100];
+int i;
+printf("enter a string: ");
+gets(str);
+for(i=0;str[i]!='\0';i++){
+if(islower(str[i]))
+str[i]=toupper(str[i]);
+else if(isupper(str[i]))
+str[i]=tolower(str[i]);
+}
+printf("toggled string: %s",str);
+return 0;
+}
