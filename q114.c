ff145c282365bfce3b9e1c462c8d85f626c44b63
adityaraturi49-ui <adityaraturi49@gmail.com>
2025-11-25 22:39:19 +0530

    Add files via upload

---
+++ q114.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    printf("Enter the string: ");
+    scanf("%s", s);
+
+    int n = strlen(s);
+    int maxLen = 0;
+
+
+    for (int i = 0; i < n; i++) {
+        int visited[256] = {0};
+        int len = 0;
+
+        for (int j = i; j < n; j++) {
+            if (visited[(int)s[j]]) {
+                break;
+            } else {
+                visited[(int)s[j]] = 1;
+                len++;
+            }
+        }
+
+        if (len > maxLen) {
+            maxLen = len;
+        }
+    }
+
+    printf("Length of longest substring without repeating characters: %d\n", maxLen);
+
+    return 0;
+}
