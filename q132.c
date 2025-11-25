0acf55c1d15c78c63392267c73f01cbfc62cac3a
adityaraturi49-ui <adityaraturi49@gmail.com>
2025-11-25 22:04:30 +0530

    Add files via upload

---
+++ q132.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+enum TrafficLight {
+    RED,
+    YELLOW,
+    GREEN
+};
+
+int main() {
+    enum TrafficLight signal;
+
+
+    signal = GREEN;
+
+    if (signal == RED) {
+        printf("Stop\n");
+    }
+    else if (signal == YELLOW) {
+        printf("Wait\n");
+    }
+    else if (signal == GREEN) {
+        printf("Go\n");
+    }
+
+    return 0;
+}
